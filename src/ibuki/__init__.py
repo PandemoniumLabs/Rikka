494045674b443f87a264541bb52845985cf1a81b
Xeon <XeonXE1@proton.me>
2026-01-15 13:38:10 +0530

    Path

--- src/ibuki/__init__.py
+++ src/ibuki/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # Base project directory
-BASE_PATH = Path(__file__).parent.parent.resolve()
+BASE_PATH = Path(__file__).parent.resolve()
 
 # CSS directory
 CSS_PATH = BASE_PATH / "css"
